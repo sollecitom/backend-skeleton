50d32122a337475af89146929d3da68da280cb98
Michele Sollecito <msollecito@meta.com>
2026-03-29 21:08:59 +0100

    WIP

--- build-logic/src/main/kotlin/sollecitom.kotlin-jvm-conventions.gradle.kts
+++ build-logic/src/main/kotlin/sollecitom.kotlin-jvm-conventions.gradle.kts
@@ -28,7 +28,6 @@ tasks.withType<KotlinCompile>().configureEach {
         freeCompilerArgs.addAll(
             "-Xjsr305=strict",
             "-Xcontext-parameters",
-            "-opt-in=kotlin.time.ExperimentalTime",
         )
     }
 }
